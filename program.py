26e8cdd082958a47b0b7e1088af31759162712d2
VictorRubinec <victor.rubinec@sptech.school>
2024-03-19 12:07:27 -0300

    v_0.2 funcionalidade de fala do bot implementada

--- program.py
+++ program.py
@@ -1,12 +1,14 @@
-from openai import OpenAI
 import json
 import sounddevice as sd
 import soundfile as sf
+from google.cloud import texttospeech
+from openai import OpenAI
 
-key = json.load(open('key.json'))['key']
-client = OpenAI(api_key=key)
+openai_key = json.load(open('openai.json'))['key']
+gctts_key = "gctts.json"
+client = OpenAI(api_key=openai_key)
 
-def record_audio(file_name, duration=5, sample_rate=44100):
+def record_audio(file_name, duration=10, sample_rate=44100):
     print("Gravando...")
     audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype='int16')
     sd.wait()
@@ -15,7 +17,7 @@ def record_audio(file_name, duration=5, sample_rate=44100):
     
 def converter_audio_texto(file_name):
     print("Convertendo áudio para texto...")
-    audio_file= open("record.wav", "rb")
+    audio_file= open(file_name, "rb")
     transcription = client.audio.transcriptions.create(
         model="whisper-1", 
         file=audio_file
@@ -28,13 +30,40 @@ def enviar_mensagem(lista_mensagens):
         messages=lista_mensagens
     )
     return response.choices[0].message.content
+
+def falar_resposta(resposta, output_file):    
+    client = texttospeech.TextToSpeechClient.from_service_account_json(gctts_key)
+    synthesis_input = texttospeech.SynthesisInput(text=resposta)
+
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="pt-BR",
+        name="pt-BR-Standard-B",
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3,
+        speaking_rate=1.15,
+        pitch=-4.5,
+    )
+
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    with open(output_file, "wb") as out:
+        out.write(response.audio_content)
+        
+    audio_data, sample_rate = sf.read(output_file, dtype='float32')
+    sd.play(audio_data, sample_rate)
+    sd.wait()
+    
     
 lista_mensagens =[
-    {"role": "system", "content": "Você é um assistente narrador."}
+    {"role": "system", "content": "Você é um funcionário da Intel que interresado em comprar processadores da empresa do usuário"},
 ]
     
 while True:
-    print("Aperter enter para gravar um áudio ou digite 'sair' para sair")
+    print("Aperte enter para gravar um áudio ou digite 'sair' para sair")
     mensagem = input()
     if mensagem == 'sair':
         break
@@ -49,3 +78,5 @@ while True:
     resposta = enviar_mensagem(lista_mensagens)
     print('Bot:', resposta)
     lista_mensagens.append({"role": "assistant", "content": resposta})
+    
+    falar_resposta(resposta, 'resposta.wav')
