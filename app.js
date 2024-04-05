102c66cfba53b136984f7c18f2fe46569174a74d
VictorRubinec <victor.rubinec@sptech.school>
2024-04-05 09:37:08 -0300

    v_1.0.8.2 - colocando a opcao de desenvolvimento e producao

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "localhost";
 const DOMAIN = process.env.DOMAIN || "localhost";
 
+const PROJETO = process.env.DEBUG || "desenvolvimento";
+
 var app = express();
 
 const indexRouter = require("./src/routes/indexRoute.js");
@@ -21,25 +23,37 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
-const base_path = process.env.BASE_PATH || "/"; 
+const base_path = process.env.BASE_PATH || "/";
 
 app.use(base_path, indexRouter);
-app.use(`${base_path}functions`, functionsRouter); 
+app.use(`${base_path}functions`, functionsRouter);
 
-const privateKeyPath = `/etc/letsencrypt/live/${DOMAIN}/privkey.pem`;
-const certificatePath = `/etc/letsencrypt/live/${DOMAIN}/fullchain.pem`;
-
-// Configurações para o servidor HTTPS
-const httpsOptions = {
-  key: fs.readFileSync(privateKeyPath),
-  cert: fs.readFileSync(certificatePath)
-};
-
-// Criando um servidor HTTPS
-const server = https.createServer(httpsOptions, app);
-
-server.listen(PORT, () => {
+if (PROJETO === "desenvolvimento") {
+  // Criando um servidor HTTP
+  app.listen(PORT, () => {
     console.log(`O servidor está rodando na porta ${PORT}`);
     console.log(`Aplicação disponível em: http://${HOST}:${PORT}`);
     console.log(`Nome do domínio: ${DOMAIN}`);
-});
+  });
+} else if (PROJETO === "produção") {
+
+  const privateKeyPath = `/etc/letsencrypt/live/${DOMAIN}/privkey.pem`;
+  const certificatePath = `/etc/letsencrypt/live/${DOMAIN}/fullchain.pem`;
+
+  // Configurações para o servidor HTTPS
+  const httpsOptions = {
+    key: fs.readFileSync(privateKeyPath),
+    cert: fs.readFileSync(certificatePath)
+  };
+
+  const server = https.createServer(httpsOptions, app);
+
+  // Criando um servidor HTTPS
+  server.listen(PORT, () => {
+    console.log(`O servidor está rodando na porta ${PORT}`);
+    console.log(`Aplicação disponível em: http://${HOST}:${PORT}`);
+    console.log(`Nome do domínio: ${DOMAIN}`);
+  });
+}
+
+
